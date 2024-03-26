97b1e02657563fb426eafc697735ad1d0ec5f1c9
Jesus Borda <bordajimenezchucho@gmail.com>
2024-03-25 20:58:19 -0600

    creacion de botones carrucel

--- src/custom/components/HomeSectondCarousel/HomeSectionCarousel.jsx
+++ src/custom/components/HomeSectondCarousel/HomeSectionCarousel.jsx
@@ -1,7 +1,8 @@
 import React from 'react'
 import AliceCarousel from 'react-alice-carousel'
 import HomeSectionCard from '../HomeSectionCard/HomeSectionCard';
-  
+import KeyboardArrowLeftIcon from '@mui/icons-material/KeyboardArrowLeft';
+import { Button } from '@mui/material';
 
 const HomeSectionCarousel = () => {
 
@@ -22,7 +23,41 @@ const HomeSectionCarousel = () => {
                     autoPlayInterval={1000}
                     infinite
                     responsive={responsive}
-                />           
+                />
+                <Button variante="contained" className="z-50 bg-white" 
+                    sx={{
+                        position:'absolute', 
+                        top:"8rem",
+                        right:"0rem", 
+                        transform:"translateX(50%) rotate(90deg)",
+                        bgcolor:"white"
+                    }} 
+                    ºaria-label="next">
+                    <KeyboardArrowLeftIcon
+                    sx={{
+                        transform:"rotate(90deg)",
+                        color:"black"
+                    }}
+                    />
+                </Button>
+
+
+                <Button variante="contained" className="z-50 bg-white" 
+                    sx={{
+                        position:'absolute', 
+                        top:"8rem",
+                        left:"0rem", 
+                        transform:"translateX(50%) rotate(-90deg)",
+                        bgcolor:"white"
+                    }} 
+                    aria-label="next">
+                    <KeyboardArrowLeftIcon
+                    sx={{
+                        transform:"rotate(90deg)",
+                        color:"black"
+                    }}
+                    />
+                </Button>
             </div>
 
         </div>
